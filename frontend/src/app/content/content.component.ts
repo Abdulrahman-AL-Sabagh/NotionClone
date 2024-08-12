da1d055171909b5d2b007d84bf8bfaa16850cf99
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2024-08-12 17:22:43 +0200

    add dropdown for adding imagesand simple start with rich text editor. Not that there is no functionality bound to the dropdown

--- frontend/src/app/content/content.component.ts
+++ frontend/src/app/content/content.component.ts
@@ -1,20 +1,52 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  signal,
+  viewChild,
+  ViewChild,
+  WritableSignal,
+} from '@angular/core';
 import { EditPageComponent } from './edit-page/edit-page.component';
 import { FormControl } from '@angular/forms';
+import { DropdownComponent } from './editor/dropdown/dropdown.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [EditPageComponent],
+  imports: [EditPageComponent, DropdownComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css',
 })
 export class ContentComponent {
   title = new FormControl();
-  content = '';
+  content: WritableSignal<string[]> = signal([]);
+  newContent = signal('');
+  isFocused = signal(false);
+  optionsVisible = true;
+
+  @ViewChild('editor') editor!: ElementRef<HTMLDivElement>;
   /**
    *
    */
+
   constructor() {}
-  handleChange(event: Event) {}
+  handleChange(event: Event) {
+    const element = event.target as HTMLDivElement;
+    console.log(element.innerText);
+  }
+
+  handleFocus() {
+    this.isFocused.set(true);
+  }
+  handleBlur() {
+    const paragraph = document.createElement('p');
+    paragraph.innerHTML = this.editor.nativeElement.innerHTML;
+    this.editor.nativeElement.innerHTML = '';
+    this.editor.nativeElement.appendChild(paragraph);
+  }
+
+  private resetAndCreateNewInput() {
+    this.content.set([...this.content(), this.newContent()]);
+    this.newContent.set('');
+  }
 }
