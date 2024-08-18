f19cb2a5c10ee923f9d2d4e8d50def2054dec151
Abdulrahman-AL-Sabagh <abdsabagh8@gmail.com>
2024-08-18 22:33:32 +0200

    create register form #7

--- frontend/src/app/register-form/register-form.component.ts
+++ frontend/src/app/register-form/register-form.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.css'
+  styleUrl: './register-form.component.css',
 })
 export class RegisterFormComponent {
-
+  form: FormGroup = new FormGroup({
+    username: new FormControl(),
+    email: new FormControl(),
+    passsword: new FormControl(),
+    repeatPassword: new FormControl(),
+  });
 }
